6edbb1ee793beb71ef1bbf61172fdede385d0056
sutthi-b <merote6384@gmail.com>
2022-09-18 08:11:46 +0700

    [02-setup] - install ui dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import 'antd/dist/antd.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
