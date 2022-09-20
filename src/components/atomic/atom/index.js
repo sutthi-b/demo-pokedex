5a8c9307054cc53d0b2c0d8249e9b2620ceb2acc
sutthi-b <merote6384@gmail.com>
2022-09-20 08:49:40 +0700

    [03-search-page] - search page - header (Logo and filter)

--- src/components/atomic/atom/index.js
+++ src/components/atomic/atom/index.js
@@ -1,3 +1,7 @@
 import Button from './Button/Button';
 import Image from './Image/Image';
-export { Button, Image };
+import Text from './Text/Text';
+import Dropdown from './Dropdown/Dropdown';
+import Menu from './Menu/Menu';
+
+export { Button, Image, Text, Dropdown, Menu };
--- src/components/atomic/page/SearchPage/SearchPage.jsx
+++ src/components/atomic/page/SearchPage/SearchPage.jsx
@@ -1,14 +1,52 @@
-import { Logo } from '@atomic';
+import styled from 'styled-components';
+import { Row, Col } from 'antd';
+
+import { Logo, FilterDropdown } from '@atomic';
+
+import { regions } from './helper';
 
 // import pokemonLogo from '../../../../assets/images/pokedex.png';
 import pokemonLogo from '@/assets/images/pokedex.png';
 
+const Container = styled.div`
+  text-align: center;
+`;
+
+const StyledRow = styled(Row)`
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 2rem;
+  padding: 2rem;
+`;
+
 const SearchPage = () => {
+  const regionsDropdownItems = regions.map((r) => {
+    return {
+      ...r,
+      key: r?.name,
+      value: r?.name,
+      label: `${r?.name} (${r?.offset} - ${r?.limit + r?.offset})`
+    };
+  });
+
   return (
-    <div>
-      Search Page
-      <Logo src={pokemonLogo} />
-    </div>
+    <Container>
+      <Logo src={pokemonLogo} width={200} />
+      <StyledRow>
+        <Col xs={24} sm={12} md={6}>
+          <FilterDropdown label="REGION" items={regionsDropdownItems} />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <FilterDropdown label="REGION" items={regionsDropdownItems} />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <FilterDropdown label="REGION" items={regionsDropdownItems} />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <div>search</div>
+        </Col>
+      </StyledRow>
+    </Container>
   );
 };
 
